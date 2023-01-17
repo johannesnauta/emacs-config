254a8a8eb36301e629f85d5744f8d29a4886bd6b
Johannes Nauta <johannes.nauta@protonmail.com>
2023-01-17 23:15:25 +0100

    Includes Git functionality
    Includes Magit (with some very minimal configuration that only makes the
    magit-status buffer full screen) and adds Hydra and a package yoinked from
    unpackaged called smerge-hydra. The latter makes resolving conflicts more
    streamlined as they will require less keypresses.

--- config.el
+++ config.el
@@ -149,6 +149,54 @@
                  nil
                  (window-parameters (mode-line-format . none)))))
 
+(use-package magit
+  :ensure t
+  :config
+  (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1))
+
+(use-package hydra
+   :ensure t)
+
+;; Stolen from https://github.com/alphapapa/unpackaged.el#hydra 
+(use-package smerge-mode
+  :config
+  (defhydra smerge-hydra
+    (:color pink :hint nil :post (smerge-auto-leave))
+    "
+^Move^       ^Keep^               ^Diff^                 ^Other^
+^^-----------^^-------------------^^---------------------^^-------
+_n_ext       _b_ase               _<_: upper/base        _C_ombine
+_p_rev       _u_pper              _=_: upper/lower       _r_esolve
+^^           _l_ower              _>_: base/lower        _k_ill current
+^^           _a_ll                _R_efine
+^^           _RET_: current       _E_diff
+"
+    ("n" smerge-next)
+    ("p" smerge-prev)
+    ("b" smerge-keep-base)
+    ("u" smerge-keep-upper)
+    ("l" smerge-keep-lower)
+    ("a" smerge-keep-all)
+    ("RET" smerge-keep-current)
+    ("\C-m" smerge-keep-current)
+    ("<" smerge-diff-base-upper)
+    ("=" smerge-diff-upper-lower)
+    (">" smerge-diff-base-lower)
+    ("R" smerge-refine)
+    ("E" smerge-ediff)
+    ("C" smerge-combine-with-next)
+    ("r" smerge-resolve)
+    ("k" smerge-kill-current)
+    ("ZZ" (lambda ()
+            (interactive)
+            (save-buffer)
+            (bury-buffer))
+     "Save and bury buffer" :color blue)
+    ("q" nil "cancel" :color blue))
+  :hook (magit-diff-visit-file . (lambda ()
+                                   (when smerge-mode
+                                     (smerge-hydra/body)))))
+
 (use-package yasnippet
   :ensure t
   :diminish yas-global-mode
@@ -293,9 +341,9 @@
   ;; "Send the current julia cell (delimited by ###) to the julia shell"
   (interactive)
   (save-excursion
-    (setq cell-begin (if (re-search-backward "^###" nil t) (point) (point-min))))
+    (setq cell-begin (if (re-search-backward "^#/" nil t) (point) (point-min))))
   (save-excursion
-    (setq cell-end (if (re-search-forward "^###" nil t) (point) (point-max))))
+    (setq cell-end (if (re-search-forward "^#/" nil t) (point) (point-max))))
   (set-mark cell-begin)
   (goto-char cell-end)
   (julia-repl-send-region-or-line)
