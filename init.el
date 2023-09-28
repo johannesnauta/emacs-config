34933a4ca972563388d8d07ac2aca640a0609d54
johannes.nauta@protonmail.com <johannes.nauta@protonmail.com>
2023-09-28 10:11:12 +0200

    Included devil mode. Included 'fuzzy' search.

--- init.el
+++ init.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(cmake-mode zenburn-theme yasnippet-snippets which-key vterm vmd-mode vertico use-package poke-line ox-pandoc org-roam org-fragtog org-bullets orderless minions markdown-preview-mode marginalia magit lua-mode lsp-latex lsp-julia lambda-line julia-repl jedi hydra exec-path-from-shell esup emojify eglot-jl diminish dashboard company citar-embark bug-hunter avy autothemer auctex all-the-icons)))
+   '(devil cmake-mode zenburn-theme yasnippet-snippets which-key vterm vmd-mode vertico use-package poke-line ox-pandoc org-roam org-fragtog org-bullets orderless minions markdown-preview-mode marginalia magit lua-mode lsp-latex lsp-julia lambda-line julia-repl jedi hydra exec-path-from-shell esup emojify eglot-jl diminish dashboard company citar-embark bug-hunter avy autothemer auctex all-the-icons))
+ '(warning-suppress-log-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
